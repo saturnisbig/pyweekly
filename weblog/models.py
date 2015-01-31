41de5bb0a86a95128f2b18cba937870a686ad66d
Teddy <i.kenting@gmail.com>
2015-01-31 22:16:37 +0800

    Add Entry model using south app

--- weblog/models.py
+++ weblog/models.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import datetime
+from markdown import markdown
+
 from django.db import models
 
 
@@ -17,3 +20,28 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return "/categories/%s/" % self.slug
+
+
+class Entry(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique_for_date="pub_date")
+    excerpt = models.TextField(blank=True)
+    content = models.TextField()
+    pub_date = models.DateTimeField(default=datetime.datetime.now)
+
+    enable_comments = models.BooleanField(default=True)
+
+    category = models.ForeignKey('Category')
+
+    excerpt_html = models.TextField(editable=False)
+    content_html = models.TextField(editable=False)
+
+    class Meta:
+        ordering = ["-pub_date"]
+        verbose_name_plural = "Entries"
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return "entries/%s/" % self.slug
