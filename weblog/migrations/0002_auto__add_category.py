313e3af2f9a6938cdd0089868c22c8e91a4aaa4d
Teddy <i.kenting@gmail.com>
2015-01-31 21:07:32 +0800

    Add Category model and active in admin app

---
+++ weblog/migrations/0002_auto__add_category.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Category'
+        db.create_table(u'weblog_category', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=50)),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'weblog', ['Category'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Category'
+        db.delete_table(u'weblog_category')
+
+
+    models = {
+        u'weblog.category': {
+            'Meta': {'object_name': 'Category'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['weblog']
--- weblog/models.py
+++ weblog/models.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(u'类别标题', max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name_plural = "Categories"
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return "/categories/%s/" % self.slug
