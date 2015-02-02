09c13875614b90374ca6ceb88fd854e652e320d8
Teddy <i.kenting@gmail.com>
2015-02-02 20:16:23 +0800

    Decouple URLs to app weblog's urls.py.

--- pyweekly/urls.py
+++ pyweekly/urls.py
@@ -3,10 +3,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from issues.views import *
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pyweekly.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^issues/$', issues_list, name='issues'),
+    url(r'^issues/(\d+)/', issue_articles_list),
+    #url(r'^blog/', include('coltrane.urls')),
+    url(r'^weblog/', include('weblog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
---
+++ weblog/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, url
+
+from weblog.views import *
+
+urlpatterns = patterns('',
+    url(r'^$', EntryArchiveIndexView.as_view()),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
+        EntryDateDetailView.as_view(), name='entry_detail'),
+    url(r'^(?P<year>\d{4})/$', EntryYearArchiveView.as_view()),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$',
+        EntryMonthArchiveView.as_view()),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$',
+        EntryDayArchiveView.as_view()),
+
+)
